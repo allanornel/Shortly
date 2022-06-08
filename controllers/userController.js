8e1667de59cb622f45f46a45301ab66c409f0dee
allanornel <allanornel@gmail.com>
2022-06-08 18:48:44 -0300

    Feat: try of ranking

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,4 +26,26 @@ export async function getUserId(req, res) {
     res.status(500).send("Ocorreu um erro ao obter o Usuário");
   }
 }
-export async function getRanking(req, res) {}
+
+export async function getRanking(req, res) {
+  try {
+    const ranking =
+      await db.query(`SELECT u.id, u.name, COUNT(l.id) AS "linksCount", SUM(l."visitCount") AS "visitCount" FROM users u
+  LEFT JOIN links l ON u.id = l."userId"
+  GROUP BY u.id
+  ORDER BY "visitCount" DESC NULLS LAST LIMIT 10`);
+
+    const resposta = ranking.map((index) => {
+      if (index.linkCount === 0) {
+        return { ...index, visitCount: 0 };
+      } else {
+        return { ...index };
+      }
+    });
+
+    res.status(200).send(ranking);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao obter o ranking");
+  }
+}
